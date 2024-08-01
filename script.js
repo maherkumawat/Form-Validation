8177dba8fe8f3c4c89533c242b32a71c4c9766a5
Maher Kumawat <murarikumawat002@gmail.com>
2024-08-01 21:36:08 +0530

    message

--- script.js
+++ script.js
@@ -1,139 +1,71 @@
+const submitBtn = document.getElementById('submitBtn');
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const passError = document.getElementById('passError');
 
-const inputBox = document.getElementById('inputBox');
-const addBtn = document.getElementById('addBtn');
-const todoList = document.getElementById('todoList');
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
 
-let editTodo = null;
+    if(validateName() && validateEmail() && validatePassword()){
+        alert("Form Submitted Successfully");
+    }
+});
 
-// Function to add todo
-const addTodo = () => {
-    const inputText = inputBox.value.trim();
-    if (inputText.length <= 0) {
-        alert("You must write something in your to do");
+
+function validateName(){
+    let name = document.getElementById('name').value;
+
+    if(name.length == 0){
+        nameError.innerHTML = "Name is required";
+        nameError.previousElementSibling.classList.add('fa-xmark');
         return false;
     }
 
-    if (addBtn.value === "Edit") {
-        // Passing the original text to editLocalTodos function before edit it in the todoList
-        editLocalTodos(editTodo.target.previousElementSibling.innerHTML);
-        editTodo.target.previousElementSibling.innerHTML = inputText;
-        addBtn.value = "Add";
-        inputBox.value = "";
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Write full Name";
+        nameError.previousElementSibling.classList.add('fa-xmark');
+        return false;
     }
-    else {
-        //Creating p tag
-        const li = document.createElement("li");
-        const p = document.createElement("p");
-        p.innerHTML = inputText;
-        li.appendChild(p);
-
-
-        // Creating Edit Btn
-        const editBtn = document.createElement("button");
-        editBtn.innerText = "Edit";
-        editBtn.classList.add("btn", "editBtn");
-        li.appendChild(editBtn);
-
-        // Creating Delete Btn
-        const deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "Remove";
-        deleteBtn.classList.add("btn", "deleteBtn");
-        li.appendChild(deleteBtn);
-
-        todoList.appendChild(li);
-        inputBox.value = "";
-
-        saveLocalTodos(inputText);
-    }
+    nameError.innerHTML = "";
+    nameError.previousElementSibling.classList.add('fa-check');
+    return true;
 }
 
-// Function to update : (Edit/Delete) todo
-const updateTodo = (e) => {
-    if (e.target.innerHTML === "Remove") {
-        todoList.removeChild(e.target.parentElement);
-        deleteLocalTodos(e.target.parentElement);
+function validateEmail(){
+    let email = document.getElementById('email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = "Email is required";
+        emailError.previousElementSibling.classList.add('fa-xmark');
+        return false;
     }
 
-    if (e.target.innerHTML === "Edit") {
-        inputBox.value = e.target.previousElementSibling.innerHTML;
-        inputBox.focus();
-        addBtn.value = "Edit";
-        editTodo = e;
+    if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+        emailError.innerHTML = "Enter Valid Email";
+        emailError.previousElementSibling.classList.add('fa-xmark');
+        return false;
     }
+    emailError.innerHTML = "";
+    emailError.previousElementSibling.classList.add('fa-check');
+    return true;
+}
+function validatePassword(){
+    let password = document.getElementById('password').value;
+
+    if(password.length == 0){
+        passError.innerHTML = "Password is required";
+        passError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+
+    if(!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,30}$/)){
+        passError.innerHTML = "Password should contain 1 Uppercase, 1 Lowecase, 1 Digit & 1 Alphabet";
+        passError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    passError.innerHTML = "";
+    passError.previousElementSibling.classList.add('fa-check');
+    return true;
 }
 
-// Function to save local todo
-const saveLocalTodos = (todo) => {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    }
-    else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// Function to get local todo
-const getLocalTodos = () => {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    }
-    else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-        todos.forEach(todo => {
-
-            //Creating p tag
-            const li = document.createElement("li");
-            const p = document.createElement("p");
-            p.innerHTML = todo;
-            li.appendChild(p);
-
-
-            // Creating Edit Btn
-            const editBtn = document.createElement("button");
-            editBtn.innerText = "Edit";
-            editBtn.classList.add("btn", "editBtn");
-            li.appendChild(editBtn);
-
-            // Creating Delete Btn
-            const deleteBtn = document.createElement("button");
-            deleteBtn.innerText = "Remove";
-            deleteBtn.classList.add("btn", "deleteBtn");
-            li.appendChild(deleteBtn);
-
-            todoList.appendChild(li);
-        });
-    }
-}
-
-// Function to delete local todo
-const deleteLocalTodos = (todo) => {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    }
-    else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-
-    let todoText = todo.children[0].innerHTML;
-    let todoIndex = todos.indexOf(todoText);
-    todos.splice(todoIndex, 1);
-    localStorage.setItem("todos", JSON.stringify(todos));
-    // Array functions : slice / splice
-    console.log(todoIndex);
-}
-
-const editLocalTodos = (todo) => {
-    let todos = JSON.parse(localStorage.getItem("todos"));
-    let todoIndex = todos.indexOf(todo);
-    todos[todoIndex] = inputBox.value;
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-document.addEventListener('DOMContentLoaded', getLocalTodos);
-addBtn.addEventListener('click', addTodo);
-todoList.addEventListener('click', updateTodo);
+// Make a validateConfirmPassword function to validate it
